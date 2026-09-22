77fdfe242d42c809f09de770f0572486225bbe6d
Jaagop Janson <dev@jaagop.eu>
2026-09-22 16:51:18 +0300

    chore: declare the package version 1.0.1

--- Sources/LagoonEngine/EngineVersion.swift
+++ Sources/LagoonEngine/EngineVersion.swift
@@ -9,7 +9,7 @@ import Libavformat
 /// contributing guide.
 public nonisolated enum EngineVersion {
     /// This package's own version, as its tag spells it.
-    public static let current = "1.0.0"
+    public static let current = "1.0.1"
 
     /// The linked libavformat, as `lavf<major>.<minor>.<micro>`.
     ///
